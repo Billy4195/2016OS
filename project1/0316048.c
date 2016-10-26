efbb7596b0373f5059b6cc9a8a84550da112ac2f
Billy.Su <g4691821@gmail.com>
2016-10-26 21:57:24 +0800

    background execute

--- project1/0316048.c
+++ project1/0316048.c
@@ -16,15 +16,65 @@ void push_arg_list(char ***arg_list, unsigned int *capacity, unsigned int *fille
         (*arg_list)[(*filled)++] = token;
 }
 
+int allocate_bg_proc_list(int **bg_pid_list_ptr,int len){
+        int *tmp = malloc( sizeof(int) * (len+10));
+        if(*bg_pid_list_ptr !=NULL){
+                free(*bg_pid_list_ptr);
+                memcpy((void*)tmp,(void*)*bg_pid_list_ptr,sizeof(int)*len);
+        }
+        *bg_pid_list_ptr = tmp;
+        return len+10;
+}
+
+void push_bg_proc_list(int **bg_pid_list_ptr,int *len,int *filled,int pid){
+        if(*filled == *len){
+                *len = allocate_bg_proc_list(bg_pid_list_ptr, 
+                                (*len) );
+        }
+        (*bg_pid_list_ptr)[*filled] = pid;
+        (*filled)++;
+}
+
+int check_bg_proc_list(int *bg_pid_list, int *filled, char block){
+        int i;
+        int j;
+        int status;
+        if(block){
+                while(*filled){
+                        wait(&status);
+                        (*filled)--;
+                }
+        }else{
+                for(i=0; i< *filled;i++){
+                        waitpid(bg_pid_list[i],&status,WNOHANG);
+                        if(WIFEXITED(status)){
+                               printf("[%d] exited\n",bg_pid_list[i]);
+                               for(j=0;j<*filled-1;j++){
+                                        bg_pid_list[j] = bg_pid_list[j+1];
+                               }
+                               bg_pid_list[*filled-1] = 0;
+                               (*filled)--;
+                        }
+                }
+        }
+        //need free list
+}
 int main(int argc, char ** argv){
         int i;
         char input[2048];
         char *token;
         char **arg_list;
         int pid;
+        int *bg_pid_list=NULL;
+        int list_len;
+        int list_filled;
         unsigned int capacity,filled;
+        char background;
+        list_len = allocate_bg_proc_list(&bg_pid_list,0);
+        list_filled = 0;
         printf("> ");
         while( fgets(input,sizeof(input),stdin) != NULL ){
+                background = 0;
                 capacity = 10;
                 filled = 0;
                 arg_list = calloc(capacity, sizeof(char*));
@@ -33,22 +83,37 @@ int main(int argc, char ** argv){
                 while( (token = strtok(NULL," \n")) != NULL){
                         push_arg_list(&arg_list, &capacity, &filled, token);
                 }
+                if( strcmp(arg_list[filled-1],"&") == 0){
+                        background =1;
+                        arg_list[filled-1] = NULL;
+                        filled--;
+                }
                 for(i=0;i<filled;i++){                  //print arg_list
                         printf("arg %d : %s\n",i,arg_list[i]);
                 }
                 switch(pid = fork()){
                         case 0:
                                 exit(execvp(arg_list[0],arg_list));
+                                
                         case -1:
                                 printf("ERROR occurred when fork child process\n");
                                 break;
                         default:
-                                wait(NULL);
+                                if(background){
+                                        printf("[%d]\n",pid);
+                                        push_bg_proc_list(&bg_pid_list, &list_len, &list_filled, pid);
+                                        check_bg_proc_list(bg_pid_list, &list_filled, 0);
+                                }else{
+                                        check_bg_proc_list(bg_pid_list, &list_filled, 0);
+                                        waitpid(pid,NULL,0);
+                                }
                 }
                 free(arg_list);
                 printf("> ");
         }
+        check_bg_proc_list(bg_pid_list, &list_filled, 1);
         printf("\n");
+        free(bg_pid_list);
         return 0;
 }
 

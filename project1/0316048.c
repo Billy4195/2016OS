3a721f4fa0af55a0848f0ea9bc25ab3c431daa72
Billy.Su <g4691821@gmail.com>
2016-10-26 16:32:07 +0800

    basic program execute
    file path to execute
    file name of /bin to execute

--- project1/0316048.c
+++ project1/0316048.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void push_arg_list(char ***arg_list, unsigned int *capacity, unsigned int *filled, char *token){
         char ** tmp_list;
         if(*filled == *capacity){
-                tmp_list = malloc( sizeof(char*) * (*capacity+10) );
+                tmp_list = calloc(*capacity+10, sizeof(char*));
                 memcpy((void*)tmp_list, (void*)*arg_list, 
                         sizeof(char*) * (*capacity));
                 free(*arg_list);
@@ -19,24 +20,43 @@ int main(int argc, char ** argv){
         int i;
         char input[2048];
         char *token;
-        char **arg_list=malloc( sizeof(char*) * 10);
+        char **arg_list;
+        char file[257];
+        int pid;
         unsigned int capacity,filled;
-        capacity = 10;
-        filled = 0;
         printf("> ");
         while( fgets(input,sizeof(input),stdin) != NULL ){
-                token = strtok(input," ");
+                capacity = 10;
+                filled = 0;
+                arg_list = calloc(capacity, sizeof(char*));
+                token = strtok(input," \n");
                 push_arg_list(&arg_list, &capacity, &filled, token);
                 while( (token = strtok(NULL," \n")) != NULL){
                         push_arg_list(&arg_list, &capacity, &filled, token);
                 }
-                for(i=0;i<filled;i++){
+                for(i=0;i<filled;i++){                  //print arg_list
                         printf("arg %d : %s\n",i,arg_list[i]);
                 }
-                filled = 0;
+                if(strstr(arg_list[0],"/") != NULL){
+                        strcpy(file, arg_list[0]);
+
+                }else{
+                        sprintf(file,"/bin/%s",arg_list[0]);
+                }
+                printf("FILE %s\n",file);
+                switch(pid = fork()){
+                        case 0:
+                                exit(execvp(file,arg_list));
+                                
+                        case -1:
+                                printf("ERROR occurred when fork child process\n");
+                                break;
+                        default:
+                                wait(NULL);
+                }
+                free(arg_list);
                 printf("> ");
         }
-        printf("\n");
-        free(arg_list);
+        return 0;
 }
 
